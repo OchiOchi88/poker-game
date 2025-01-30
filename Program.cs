2f8d82ff79599691d0eaf5511224b798c380b4f2
OchiOchi88 <241203@st.yoshida-g.ac.jp>
2025-01-30 11:34:41 +0900

    1～4の数字を入力するフォームを4つ用意した

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace PokerGame
     {
         static void Main(string[] args)
         {
+            int[] number = new int[4];
+            bool check = false;
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"{i+1}つめの数字を入力してください。(1～4): ");
+                check = int.TryParse(Console.ReadLine(), out number[i]);
+                if (!check || number[i]>4 || number[i]<1)
+                {
+                    Console.WriteLine("その入力は無効です");
+                    i--;
+                }
+            }
         }
     }
 }
